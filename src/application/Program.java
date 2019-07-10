7d732a8441f21e2baf81519717d02d5e17be5d2c
lucasmss16 <lucasmss16@gmail.com>
2019-07-10 14:50:41 -0300

    update interface

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,6 @@
 package application;
 
-import java.util.Date;
+import java.util.Date; 
 
 import model.entities.Department;
 import model.entities.Seller;
@@ -12,6 +12,8 @@ public class Program {
 		Department obj = new Department(1, "Books");
 		
 		Seller seller = new Seller(21, "Lucas", "lucasmss16@gmail.com", new Date(), 3000, obj);
+		
+		
 		
 		System.out.println(seller);
 	}
