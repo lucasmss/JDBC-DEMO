269dbaa5f0877e16150c7d62d69fd78c97602c64
lucasmss16 <lucasmss16@gmail.com>
2019-07-10 15:42:04 -0300

    FindBiId

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,19 @@
 package application;
 
-import java.util.Date; 
+import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+	
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Department obj = new Department(1, "Books");
-		
-		Seller seller = new Seller(21, "Lucas", "lucasmss16@gmail.com", new Date(), 3000, obj);
-		
-		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 	}
